bd00b4807957edd1f011144f83e0af60c90625a1
inesmaria08 <inesmaria.moraru@gmail.com>
2024-09-13 13:25:22 +0300

    multi_servo application

--- examples/servo/main.c
+++ examples/servo/main.c
@@ -12,19 +12,23 @@ int main(void) {
   }
 
   uint16_t angle = 0;
+  uint16_t index = 1; // the first index available.
 
   // Changes the angle of the servomotor from 0 to 180 degrees (waiting 0.1 ms between every change).
   for (int i = 0; i <= 180; i++) {
-    if (libtock_servo_angle(i) == RETURNCODE_SUCCESS) {
+    if (libtock_servo_angle(index, i) == RETURNCODE_SUCCESS) {
       libtocksync_alarm_delay_ms(100);
       // Verifies if the function successfully returned the current angle.
-      if (libtock_current_servo_angle(&angle) == RETURNCODE_SUCCESS) {
-        printf("The current angle is: %d\n", angle);
+      if (libtock_current_servo_angle(index, &angle) == RETURNCODE_SUCCESS) {
+        printf("\nThe current angle is: %d", angle);
       } else {
         printf("\nThe servomotor is OFF\n");
       }
     } else {
       printf("\nThe angle could not be changed\n");
+      if (libtock_servo_angle(index, i) == RETURNCODE_ENODEVICE) {
+        printf("\n The index number is bigger than the available servomotors\n");
+      }
       return -1;
     }
   }
